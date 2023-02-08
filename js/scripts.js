b7d70f8780661f034754e9afb23d286a17ceec6e
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-02-08 10:00:02 +0100

    Adding files to project

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+let myName = 'Test';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+alert('Hello world')
