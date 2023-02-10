09881c434cf4b60f2282adac0a0a81189f69c7c4
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-02-10 11:26:54 +0100

    Updating script js
    Added for loop that prints Pokemon in the array.

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,15 @@ let pokemonList = [
     }
 ];
 
-console.log(pokemonList)
+// For loop goes through each item in the list and prints the name and height to the DOM with spaces
+for (let i = 0; i < pokemonList.length; i++) {
+    let size = "";
+    if (pokemonList[i].height > 5) {
+        size = "Large scale pokemon!";
+    }
+
+    // Write title, List of names, heights and extra copy if the pokemon is large scale
+    document.write("<h1>" + "Pokemon List" + "</h1>");
+    document.write("<h2>" + pokemonList[i].name + "</h2>");
+    document.write("<p>" + " Height: " + "<b>" + pokemonList[i].height + "</b>" + " " + size + "</p>");
+}
